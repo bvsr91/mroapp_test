d62a616440d5140221a1be6a80c2e5f966c1777c
Buthukuri Venkata Srinivasa Reddy <b.b.srinivasa.reddy@accenture.com>
2022-06-27 08:28:24 +0000

    sandbox and main app integration changes

--- app/mroui_fe/webapp/Component.js
+++ app/mroui_fe/webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/Device",
-    "./model/models",
-    "./controller/ErrorHandler"
-], function (UIComponent, Device, models, ErrorHandler) {
+    "./model/models"
+], function (UIComponent, Device, models) {
     "use strict";
 
     return UIComponent.extend("com.ferre.mrouife.Component", {
@@ -22,9 +21,6 @@ sap.ui.define([
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // initialize the error handler with the component
-            this._oErrorHandler = new ErrorHandler(this);
-
             // set the device model
             this.setModel(models.createDeviceModel(), "device");
 
@@ -39,7 +35,7 @@ sap.ui.define([
          * @override
          */
         destroy : function () {
-            this._oErrorHandler.destroy();
+            // this._oErrorHandler.destroy();
             // call the base component's destroy function
             UIComponent.prototype.destroy.apply(this, arguments);
         },
--- app/mroui_fe/webapp/model/formatter.js
+++ app/mroui_fe/webapp/model/formatter.js
@@ -1,21 +1,39 @@
 sap.ui.define([], function () {
     "use strict";
-
+  
     return {
-
-        /**
-         * Rounds the number unit value to 2 digits
-         * @public
-         * @param {string} sValue the number string to be rounded
-         * @returns {string} sValue with 2 digits rounded
-         */
-        numberUnit : function (sValue) {
-            if (!sValue) {
-                return "";
-            }
-            return parseFloat(sValue).toFixed(2);
+      /**
+       * Rounds the currency value to 2 digits
+       *
+       * @public
+       * @param {string} sValue value to be formatted
+       * @returns {string} formatted currency value with 2 digits
+       */
+      getBundleText: function (sId) {
+        return this.getOwnerComponent()
+          .getModel("i18n")
+          .getResourceBundle()
+          .getText(sId);
+      },
+      countryFormat: function (sCountry, sCountryCode) {
+        if (sCountryCode !== "" && sCountryCode) {
+          return sCountry + " (" + sCountryCode + ")";
+        } else {
+          return sCountry;
         }
-
+      },
+  
+      statusState: function (status) {
+          if (status === "Approved") {
+              return "Success";
+          } else if (status === "Pending") {
+              return "Warning";
+          } else if (status === "Rejected") {
+              return "Error";
+          } else {
+              return "None";
+          }
+      }
     };
-
-});
+  });
+  
